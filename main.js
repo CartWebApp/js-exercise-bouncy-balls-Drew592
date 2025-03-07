83aa421ec6a7ca92a05abf682145489c839291d3
Cornmog <drew.rosa24@student.cart.org>
2025-03-07 15:48:38 -0800

    Bouncy
    Bouncy really really bouncy

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
+
+
 // function to generate random number
 
 function random(min, max) {
@@ -25,7 +27,7 @@ function Ball(x, y, velX, velY, color, size) {
 Ball.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.arc(this.x, this.y, this.size, 2, 2 * Math.PI);
   ctx.fill();
 }
 
@@ -95,6 +97,8 @@ Ball.prototype.collisionDetect = function() {
     }
   }
 }
+
+
 
 loop();
 
